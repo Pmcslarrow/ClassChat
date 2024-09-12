ca5472673385a4ce1dc45522b2f7f580b3b9f7b2
Paul McSlarrow <pmcslarrow@icloud.com>
2024-09-12 12:22:04 -0500

    Started the new classroom page and added more constants to css styling

--- ClassChat/src/views/NewClassroom.tsx
+++ ClassChat/src/views/NewClassroom.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Logo from "./Logo";
+import { Link } from "react-router-dom";
 
 interface Question {
     question: string;
@@ -12,12 +13,24 @@ export default function NewClassroom() {
     return (
         <>
         <Logo />
-        <div className="flex-col">
+        <div className="flex-col pad">
             <h2>Discussion Question:</h2>
-            <textarea />
+            <textarea id='discussion' placeholder="Enter here..."></textarea>
+
             <h2>The Answer:</h2>
-            <input className="input-box"></input>
+            <textarea id='answer' placeholder="Enter here..."></textarea>
         </div>
+        <div className='flex-col'>
+            <div className='pad flex-row flex-center gap separate'>
+                <div className='flex-row gap'>
+                    <Link to='/' className="button">BACK</Link>
+                    <Link to='/' className="button bg-g">NEW QUESTION</Link>
+                </div>
+               
+                <Link to='/' className="button bg-o">CREATE CLASSROOM</Link>
+            </div>
+        </div>
+
         </>
     )
 }
